c33618b24f92b2b23f1267afbc76033ecaba6bcc
Rajinder <testerdv3@gmail.com>
2022-04-18 14:34:24 +0530

    checking diff

--- class1.java
+++ class1.java
@@ -1,6 +1,5 @@
 public class class1{
 public static void main(String args[]){
 System.out.println("Hi! ji");
-{}
 }
 }
--- class2.java
+++ class2.java
@@ -1,5 +1,5 @@
 public class class2{
 public static void main(String args[]){
-System.out.println("This is second class agains");
+System.out.println("This is second class");
 }
 }
